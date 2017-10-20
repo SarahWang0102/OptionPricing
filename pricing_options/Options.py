95e461bc89a70b2cb590e4973c0832a782655f8a
Sarah Wang <Sarah Wang>
2017-10-20 16:28:13 +0800

    completed 50etf delta hedge

--- pricing_options/Options.py
+++ pricing_options/Options.py
@@ -7,8 +7,8 @@ class OptionPlainEuropean:
         self.strike = strike
         self.maturitydt = maturitydt
         self.optionType = optionType
-        exercise = ql.EuropeanExercise(self.maturitydt)
-        payoff = ql.PlainVanillaPayoff(self.optionType, self.strike)
+        exercise = ql.EuropeanExercise(maturitydt)
+        payoff = ql.PlainVanillaPayoff(optionType, strike)
         option = ql.EuropeanOption(payoff, exercise)
         self.option_ql = option
 
@@ -26,3 +26,23 @@ class OptionBarrierEuropean:
         self.payoff = payoff
         barrieroption = ql.BarrierOption(barrierType, barrier, 0.0, payoff, exercise)
         self.option_ql = barrieroption
+
+class OptionPlainAmerican:
+    def __init__(self, strike,effectivedt, maturitydt, optionType):
+        self.strike = strike
+        self.maturitydt = maturitydt
+        self.optionType = optionType
+        exercise = ql.AmericanExercise(effectivedt,maturitydt)
+        payoff = ql.PlainVanillaPayoff(optionType, strike)
+        option = ql.EuropeanOption(payoff, exercise)
+        self.option_ql = option
+
+class OptionPlainAsian:
+    def __init__(self, strike,effectivedt, maturitydt, optionType):
+        self.strike = strike
+        self.maturitydt = maturitydt
+        self.optionType = optionType
+        exercise = ql.EuropeanExercise(maturitydt)
+        payoff = ql.PlainVanillaPayoff(optionType, strike)
+        option = ql.DiscreteAveragingAsianOption(payoff, exercise,)
+        self.option_ql = option
